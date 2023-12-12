6116d619747b89970366a39d14474ae2aec38507
Mohamud Nur <Mohamud-99@hotmail.com>
2023-12-12 12:12:04 +0100

    Add project files.

---
+++ day3advcode2023/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Xml;
+
+string filePath = "input.txt";
+string[] lines = File.ReadAllLines(filePath);
+int match = 0;
+int pointValue = 0;
+
+
+foreach (var line in lines)
+{
+    bool itsAMatch = false;
+    var cardInfo = line.Split(":");
+    var cardGames = cardInfo[1].Split("|");
+    var winningCards = cardGames[0].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    var myCards = cardGames[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    match = 0;
+    
+
+
+    foreach (var card in myCards)
+    {
+        
+        foreach (var winning in winningCards) 
+        {
+            if (winning == card)
+            {                
+                if(itsAMatch) 
+                {
+                    match = match * 2;
+                }
+                else
+                {
+                    match++;
+                }
+                itsAMatch = true;
+                
+            }
+        }
+        
+    }
+    pointValue += match;
+}
+
+// Del 1 Svar
+Console.WriteLine("Den totala poängen blir " + pointValue);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day3advcode2023part2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Xml;
+// Denna uppgift inte färdig ännu
+string filePath = "input.txt";
+string[] lines = File.ReadAllLines(filePath);
+int match = 0;
+int pointValue = 0;
+
+
+foreach (var line in lines)
+{
+    
+    var cardInfo = line.Split(":");
+    var cardGames = cardInfo[1].Split("|");
+    var winningCards = cardGames[0].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    var myCards = cardGames[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    match = 0;
+    int copiesCount = 0;
+
+
+    foreach (var card in myCards)
+    {
+
+        foreach (var winning in winningCards)
+        {
+            if (winning == card)
+            {
+                copiesCount++;
+            }
+        }
+
+    }
+    
+}
+
+
